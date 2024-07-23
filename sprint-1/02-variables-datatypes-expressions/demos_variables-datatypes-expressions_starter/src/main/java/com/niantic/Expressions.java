6527fd3d58c8b7185b16f2bdba3599e7dfe5d8b8
Tabatha <thabatabetts@gmail.com>
2024-07-23 14:47:32 -0700

    sprint day 2

--- sprint-1/02-variables-datatypes-expressions/demos_variables-datatypes-expressions_starter/src/main/java/com/niantic/Expressions.java
+++ sprint-1/02-variables-datatypes-expressions/demos_variables-datatypes-expressions_starter/src/main/java/com/niantic/Expressions.java
@@ -4,6 +4,7 @@ public class Expressions
 {
     public static void main(String[] args)
     {
-
+        //Calculate how many donuts per student
+        System.out.println("Total donuts: " ");
     }
 }
