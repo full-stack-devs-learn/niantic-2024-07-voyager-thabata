20bf3ff3ad5bcf759921e4aa7fb708445b15d160
Tabatha <thabatabetts@gmail.com>
2024-08-02 15:30:43 -0700

    database project (in progress)

--- sprint-1/09-database-design/exercise_database-design/family_budget.sql
+++ sprint-1/09-database-design/exercise_database-design/family_budget.sql
@@ -75,7 +75,50 @@ INSERT INTO person (person_name) VALUES ('Tabatha'), ('Raymond');
 INSERT INTO categories (category_name) VALUES ('Entertainment'), ('Food'), ('Healthcare'), ('Housing'), ('Transportation'), ('Vacation/Travel');
 
 
-INSERT INTO sub_categories (sub_category_name, category_id) VALUES ('Internet', 4), ('Rent', 4), ('Utilities', 4), ('Gas', 5), ('Groceries', 2), ('Rx', 3), ('Games', 1), ('Flight', 6), ('Uber', 5), ('Restaurant', 2), ('Dr Visit', 3), ('Streaming Services', 1), ('Hotel', 6), ('Car maintanence', 5), ('Snacks', 2), ('OTC medicine', 3);
+INSERT INTO sub_categories (sub_category_name, category_id) VALUES ('Internet', 4), ('Rent', 4), ('Utilities', 4), ('Gas', 5), ('Groceries', 2), ('Rx', 3), ('Games', 1), ('Flight', 6), ('Uber', 5), ('Restaurant', 2), ('Dr Visit', 3), ('Streaming Services', 1), ('Hotel', 6), ('Car maintenance', 5), ('Snacks', 2), ('OTC medicine', 3), ('TS Merch', 1), ('Misc', 1);
 
 
-INSERT INTO vendors (vendor_name) VALUES ('Amazon'), ('Target'), ('Fred Meyer'), ('Walgreens'), ('Dollar Tree'), ('Landmark'), ('PSE'), ('Hulu'), ('Discovery+'), ('Steam'), ('Final Fantasy XIV'), ('Nintendo'), ('Peacock'), ('Bellingahm Bay Dental'), ('Mount Baker Dental'), ('5 guys'), ('Taylor Swift Store'), ('Etsy'), ('Costco'), ('Insomnia Cookies'), ('Comcast'), ('Clarity Mental Health'), ('Visible'), ('Dairy Queen'), ('Spotify'), ('Taco Bell');
+INSERT INTO vendors (vendor_name) VALUES ('Amazon'), ('Target'), ('Fred Meyer'), ('Walgreens'), ('Dollar Tree'), ('Landmark'), ('PSE'), ('Hulu'), ('Discovery+'), ('Steam'), ('Final Fantasy XIV'), ('Nintendo'), ('Peacock'), ('Bellingham Bay Dental'), ('Mount Baker Dental'), ('5 guys'), ('Taylor Swift Store'), ('Etsy'), ('Costco'), ('Insomnia Cookies'), ('Comcast'), ('Clarity Mental Health'), ('Visible'), ('Dairy Queen'), ('Spotify'), ('Taco Bell');
+
+
+
+-- ADD RANDOM EXPENSES TO THE DATABASE:
+INSERT INTO expenses(person_id, expense_date, expense_amount, sub_category_id, vendor_id, notes) 
+VALUES
+	(1, '2024-07-02', 150, 18, 1, 'misc')
+    , (2, '2024-07-01', 17, 7, 11, 'FFXIV subscription')
+    , (1, '2024-07-02', 150, 17, 17, 'folklore cardigan')
+    , (1, '2024-07-01', 2087, 2, 6, 'rent')
+    , (2, '2024-07-03', 7.99, 10, 24, 'lunch')
+    , (1, '2024-07-05', 40, 4, 19, 'gas')
+    , (1, '2024-07-08', 5.25, 15, 5, 'candy')
+    , (1, '2024-07-11', 160, 11, 15, 'dentist')
+    , (1, '2024-07-13', 95, 6, 4, 'refills')
+    , (1, '2024-07-22', 30, 10, 16, 'dinner') 
+    , (2, '2024-07-07', 9.99, 10, 26, 'lunch')
+    , (2, '2024-07-12', 80, 1, 21, 'internet')
+    , (2, '2024-07-14', 65, 7, 1, 'video game')
+    , (2, '2024-07-25', 190, 5, 3, 'groceries')
+    , (2, '2024-07-31', 20, 12, 8, 'Hulu subscription');
+
+
+    
+-- VARIABLE TO ADD NEW EXPENSES:
+SET @person_name = 'Tabatha';
+SET @expense_date = '2024-07-30';
+SET @expense_amount = 17.89;
+SET @vendor_name = 'Dairy Queen';
+SET @notes = 'snack';
+SET @sub_category_name = 'Restaurant';
+SET @expense_id = LAST_INSERT_ID();
+
+SELECT person_id INTO @person_id FROM person WHERE person_name = @person_name;
+SELECT vendor_id INTO @vendor_id FROM vendors WHERE vendor_name = @vendor_name;
+SELECT sub_category_id INTO @sub_category_id FROM sub_categories WHERE sub_category_name = @sub_category_name;
+
+INSERT INTO expenses (person_id, expense_date, expense_amount, sub_category_id, vendor_id, notes)
+VALUES (@person_id, @expense_date, @expense_amount, @sub_category_id, @vendor_id, @notes);
+
+
+
+-- GENERATE REPORT:
