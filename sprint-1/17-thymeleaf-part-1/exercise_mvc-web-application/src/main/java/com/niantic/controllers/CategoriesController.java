1494690afdef7e041dc89bf674cbaf59f62ac258
Tabatha <thabatabetts@gmail.com>
2024-08-15 01:01:04 -0700

    exercise thymeleaf part 1

--- sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/CategoriesController.java
+++ sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/CategoriesController.java
@@ -1,10 +1,39 @@
 package com.niantic.controllers;
 
+import com.niantic.models.Category;
 import com.niantic.services.CategoryDao;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 @Controller
 public class CategoriesController
 {
-    CategoryDao categoryDao = new CategoryDao();
+    private CategoryDao categoryDao = new CategoryDao();
+
+    @GetMapping("/categories")
+    public String getCategories(Model model)
+    {
+        ArrayList<Category> categories = categoryDao.getCategories();
+
+        model.addAttribute("categories", categories);
+
+        return "categories/index";
+    }
+
+    @GetMapping("/categories/{id}")
+    public String details(Model model, @PathVariable int id)
+    {
+        ArrayList<Category> categories;
+
+        var category = categoryDao.getCategoryById(id);
+        model.addAttribute("categories", category);
+
+        return "categories/details";
+    }
 }
+
