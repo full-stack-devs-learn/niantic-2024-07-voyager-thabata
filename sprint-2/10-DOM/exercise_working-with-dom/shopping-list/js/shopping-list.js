f95172ea50894c6b091fa71ff59380a74195cb72
Tabatha <thabatabetts@gmail.com>
2024-09-03 14:14:47 -0700

    required DOM exercise completed (hopefully)

--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
@@ -1,17 +1,30 @@
-// add pageTitle
-
-// add groceries
+const service = new ShoppingService();
 
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function displayListTitle() {}
+function displayListTitle() {
+    const listTitle = document.getElementById("task-header");
+    listTitle.textContent = service.getListTitle();
+}
 
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayGroceries() {
+    const groceries = service.getGroceries()
+                         .map(grocery => {
+                            return {
+                            }
+                         });
+
+    const groceryListContainer = document.getElementById("todos");
+
+    groceries.forEach(grocery => {
+        createGroceryDiv(grocery, groceryListContainer);
+    });    
+}
 
 /**
  * This function will be called when the button is clicked. You will need to get a reference
@@ -23,4 +36,3 @@ function markCompleted() {
 
 displayListTitle();
 displayGroceries();
-
--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-service.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-service.js
@@ -15,8 +15,57 @@ class ShoppingService {
                 id: 2,
                 title: 'Frozen Pizza',
                 quantity: 4,
-                isComplete: true
+                isComplete: false
+            },
+            {
+                id: 3,
+                title: 'Gallon of Milk',
+                quantity: 2,
+                isComplete: false
+            },
+            {
+                id: 4,
+                title: 'Dozen Eggs',
+                quantity: 1,
+                isComplete: false
+            },
+            {
+                id: 5,
+                title: 'Loaf of Bread',
+                quantity: 3,
+                isComplete: false
+            },
+            {
+                id: 6,
+                title: 'Chicken Nuggets',
+                quantity: 2,
+                isComplete: false
+            },
+            {
+                id: 7,
+                title: 'Apples',
+                quantity: 6,
+                isComplete: false
+            },
+            {
+                id: 8,
+                title: 'Bananas',
+                quantity: 6,
+                isComplete: false
+            },
+            {
+                id: 9,
+                title: 'Box of Cereal',
+                quantity: 2,
+                isComplete: false
+            },
+            {
+                id: 10,
+                title: 'Olive Oil',
+                quantity: 1,
+                isComplete: false
             }
+
         ];
     }
 }
