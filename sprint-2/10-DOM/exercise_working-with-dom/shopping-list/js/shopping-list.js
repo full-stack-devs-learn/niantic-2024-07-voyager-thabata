f86c4483af94654079cf2951f8c3d276c4861441
Tabatha <thabatabetts@gmail.com>
2024-09-04 16:00:53 -0700

    ok NOW it should be good, DOM exercise completed

--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
@@ -1,17 +1,53 @@
-// add pageTitle
-
-// add groceries
-
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function displayListTitle() {}
+function displayListTitle() {
+    const listTitle = document.getElementById("title");
+    const service = new ShoppingService();
+    listTitle.textContent = service.getListTitle();
+}
 
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayShoppingList() {
+    const service = new ShoppingService();
+    const shoppingList = service.getShoppingList();
+    const shoppingListContainer = document.getElementById("shopping-list");
+
+    shoppingList.forEach(item => {
+        createListItem(item, shoppingListContainer);
+    });    
+}
+
+
+function createListItem(item, parent){
+    const listItem = document.createElement("div");
+    listItem.classList.add("list-item");
+
+    buildListItem(item, listItem);
+    buildQuantity(item, listItem);
+
+    parent.appendChild(listItem);
+}
+
+
+function buildListItem (item, parent){
+    const itemTitle = document.createElement("div");
+    itemTitle.classList.add("item");
+    itemTitle.textContent = item.title;
+    parent.appendChild(itemTitle);
+}
+
+
+function buildQuantity (item, parent){
+    const itemQuantity = document.createElement("div");
+    itemQuantity.classList.add("quantity");
+    itemQuantity.textContent = `Quantity: ${item.quantity}`;
+    parent.appendChild(itemQuantity);
+}
+
 
 /**
  * This function will be called when the button is clicked. You will need to get a reference
@@ -21,6 +57,7 @@ function markCompleted() {
 }
 
 
+document.addEventListener("DOMContentLoaded", () => {
 displayListTitle();
-displayGroceries();
-
+displayShoppingList();
+});
--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-service.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-service.js
@@ -1,21 +1,70 @@
 class ShoppingService {
 
-    getListName() {
+    getListTitle() {
+        return 'Shopping List';
     }
 
     getShoppingList() {
         return [
             {
                 id: 1,
-                title: '5 lb sack of potatoes',
-                quantity: 1,
+                title: 'Zucchini',
+                quantity: 7,
                 isComplete: false
             },
             {
                 id: 2,
                 title: 'Frozen Pizza',
-                quantity: 4,
-                isComplete: true
+                quantity: 2,
+                isComplete: false
+            },
+            {
+                id: 3,
+                title: 'Gallon of Milk',
+                quantity: 2,
+                isComplete: false
+            },
+            {
+                id: 4,
+                title: 'Dozen Eggs',
+                quantity: 1,
+                isComplete: false
+            },
+            {
+                id: 5,
+                title: 'Loaf of Bread',
+                quantity: 3,
+                isComplete: false
+            },
+            {
+                id: 6,
+                title: 'Chicken Nuggets',
+                quantity: 2,
+                isComplete: false
+            },
+            {
+                id: 7,
+                title: 'Apples',
+                quantity: 6,
+                isComplete: false
+            },
+            {
+                id: 8,
+                title: 'Bananas',
+                quantity: 6,
+                isComplete: false
+            },
+            {
+                id: 9,
+                title: 'Box of Cereal',
+                quantity: 2,
+                isComplete: false
+            },
+            {
+                id: 10,
+                title: 'Olive Oil',
+                quantity: 1,
+                isComplete: false
             }
         ];
     }
